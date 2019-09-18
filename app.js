604548a22d10fd7cdf54b934fc9bcc2233e2eeac
Taiwo <oyeniyi4real@gmail.com>
2019-09-18 22:15:15 +0100

    my login/signup task

---
+++ app.js
@@ -0,0 +1,10 @@
+const users = [];
+class UserInfo {}
+
+const firstname = document.querySelector("#firstname").value;
+const lastname = document.querySelector("#lastname").value;
+const email = document.querySelector("#email").value;
+const numb = document.querySelector("#numb").value;
+const username = document.querySelector("#username").value;
+const pword = document.querySelector("#pword").value;
+console.log(`${firstname} ${lastname} ${username} ${pword}`);
